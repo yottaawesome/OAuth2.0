8b6f585eb6d709e454859e6bd409c799826f62cb
Vasilios Magriplis <etameson@gmail.com>
2019-04-16 19:07:51 +0930

    * Continue implementation: ** Move client-id to file and templatize it. ** Clean up.

--- src/project.py
+++ src/project.py
@@ -8,6 +8,9 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 import random, string
 
+with open('clientid') as f:
+  client_id = f.read()
+
 app = Flask(__name__)
 
 #Connect to Database and create database session
@@ -22,27 +25,28 @@ def showLogin():
   state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                   for x in range(32))
   login_session['state'] = state
-  return "The current session state is {}".format(login_session['state'])
+  return render_template("login.html", client_id=client_id)
+  # return "The current session state is {}".format(login_session['state'])
 
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
-    return jsonify(MenuItems=[i.serialize for i in items])
+  restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+  items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+  return jsonify(MenuItems=[i.serialize for i in items])
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
-    Menu_Item = session.query(MenuItem).filter_by(id = menu_id).one()
-    return jsonify(Menu_Item = Menu_Item.serialize)
+  Menu_Item = session.query(MenuItem).filter_by(id = menu_id).one()
+  return jsonify(Menu_Item = Menu_Item.serialize)
 
 
 @app.route('/restaurant/JSON')
 def restaurantsJSON():
-    restaurants = session.query(Restaurant).all()
-    return jsonify(restaurants= [r.serialize for r in restaurants])
+  restaurants = session.query(Restaurant).all()
+  return jsonify(restaurants= [r.serialize for r in restaurants])
 
 
 #Show all restaurants
