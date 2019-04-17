e5145edd4c45c3e3ab7cf1b3fea9617bef5df218
Vasilios Magriplis <etameson@gmail.com>
2019-04-17 13:29:43 +0930

    Fix bugs in Udacity oauth code

--- src/project.py
+++ src/project.py
@@ -21,7 +21,6 @@ app = Flask(__name__)
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 # Create anti-forgery state token
 @app.route('/login')
@@ -56,6 +55,7 @@ def gconnect():
 
   # Check that the access token is valid.
   access_token = credentials.access_token
+  print('Got access token: {}'.format(access_token))
   url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
           % access_token)
   h = httplib2.Http()
@@ -79,22 +79,25 @@ def gconnect():
       response = make_response(
           json.dumps("Token's client ID does not match app's."), 401)
       print("Token's client ID does not match app's.")
-      print(CLIENT_ID)
-      print(result['issued_to'])
       response.headers['Content-Type'] = 'application/json'
       return response
 
   stored_access_token = login_session.get('access_token')
   stored_gplus_id = login_session.get('gplus_id')
   if stored_access_token is not None and gplus_id == stored_gplus_id:
-      response = make_response(json.dumps('Current user is already connected.'),
-                                200)
-      response.headers['Content-Type'] = 'application/json'
-      return response
+    print('Current token: {}'.format(stored_access_token))
+    print('New token: {}'.format(credentials.access_token))
+    revoke_token(login_session['access_token'])
+    login_session['access_token'] = credentials.access_token
+    response = make_response(json.dumps('Current user is already connected.'),
+                              200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
   # Store the access token in the session for later use.
   login_session['access_token'] = credentials.access_token
   login_session['gplus_id'] = gplus_id
+  login_session['token_expiry'] = credentials.token_expiry
 
   # Get user info
   userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
@@ -118,102 +121,186 @@ def gconnect():
   print("done!")
   return output
 
+def revoke_token(access_token):
+  response = requests.post('https://accounts.google.com/o/oauth2/revoke',
+    params={'token': access_token},
+    headers = {'content-type': 'application/x-www-form-urlencoded'})
+  if response.status_code == 200:
+    print('Token successfully revoked')
+  else:
+    print('Token revocation failed with status {}'.format(response.status_code))
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+  access_token = login_session.get('access_token')
+  if access_token is None:
+    print('Access Token is None')
+    response = make_response(json.dumps('Current user not connected.'), 401)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+  print('In gdisconnect access token is {}'.format(access_token))
+  print('User name is {}'.format(login_session['username']))
+  url = 'https://accounts.google.com/o/oauth2/revoke?token={}'.format(login_session['access_token'])
+  print(url)
+  h = httplib2.Http()
+  result = h.request(url, 'POST', headers={'content-type': 'application/x-www-form-urlencoded'})[0]
+  print('result is ')
+  print(result)
+
+  if result['status'] == '200':
+    del login_session['access_token']
+    del login_session['gplus_id']
+    del login_session['username']
+    del login_session['email']
+    del login_session['picture']
+    response = make_response(json.dumps('Successfully disconnected.'), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+  else:
+    response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
-  restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-  items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
-  return jsonify(MenuItems=[i.serialize for i in items])
+  try:
+    session = DBSession()
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+  finally:
+    if session:
+      session.close()
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
-  Menu_Item = session.query(MenuItem).filter_by(id = menu_id).one()
-  return jsonify(Menu_Item = Menu_Item.serialize)
+  try:
+    session = DBSession()
+    Menu_Item = session.query(MenuItem).filter_by(id = menu_id).one()
+    return jsonify(Menu_Item = Menu_Item.serialize)
 
+  finally:
+    if session:
+      session.close()
 
 @app.route('/restaurant/JSON')
 def restaurantsJSON():
-  restaurants = session.query(Restaurant).all()
-  return jsonify(restaurants= [r.serialize for r in restaurants])
+  try:
+    session = DBSession()
+    restaurants = session.query(Restaurant).all()
+    return jsonify(restaurants= [r.serialize for r in restaurants])
+  finally:
+    if session:
+      session.close()
 
 
 #Show all restaurants
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
-  restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
-  return render_template('restaurants.html', restaurants = restaurants)
-
+  try:
+    session = DBSession()
+    restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
+    return render_template('restaurants.html', restaurants = restaurants)
+  finally:
+    if session:
+        session.close()
 
 #Create a new restaurant
 @app.route('/restaurant/new/', methods=['GET','POST'])
 def newRestaurant():
-  if request.method == 'POST':
-      newRestaurant = Restaurant(name = request.form['name'])
-      session.add(newRestaurant)
-      flash('New Restaurant %s Successfully Created' % newRestaurant.name)
-      session.commit()
-      return redirect(url_for('showRestaurants'))
-  else:
-      return render_template('newRestaurant.html')
-
+  try:
+    session = DBSession()
+    if request.method == 'POST':
+        newRestaurant = Restaurant(name = request.form['name'])
+        session.add(newRestaurant)
+        flash('New Restaurant %s Successfully Created' % newRestaurant.name)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newRestaurant.html')
+  finally:
+      if session:
+        session.close()
 
 #Edit a restaurant
 @app.route('/restaurant/<int:restaurant_id>/edit/', methods = ['GET', 'POST'])
 def editRestaurant(restaurant_id):
-  editedRestaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-  if request.method == 'POST':
-      if request.form['name']:
-        editedRestaurant.name = request.form['name']
-        flash('Restaurant Successfully Edited %s' % editedRestaurant.name)
-        return redirect(url_for('showRestaurants'))
-  else:
-    return render_template('editRestaurant.html', restaurant = editedRestaurant)
-
+  try:
+    session = DBSession()
+    editedRestaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+          editedRestaurant.name = request.form['name']
+          flash('Restaurant Successfully Edited %s' % editedRestaurant.name)
+          return redirect(url_for('showRestaurants'))
+    else:
+      return render_template('editRestaurant.html', restaurant = editedRestaurant)
+  finally:
+    if session:
+      session.close()
 
 #Delete a restaurant
 @app.route('/restaurant/<int:restaurant_id>/delete/', methods = ['GET','POST'])
 def deleteRestaurant(restaurant_id):
-  restaurantToDelete = session.query(Restaurant).filter_by(id = restaurant_id).one()
-  if request.method == 'POST':
-    session.delete(restaurantToDelete)
-    flash('%s Successfully Deleted' % restaurantToDelete.name)
-    session.commit()
-    return redirect(url_for('showRestaurants', restaurant_id = restaurant_id))
-  else:
-    return render_template('deleteRestaurant.html',restaurant = restaurantToDelete)
+  try:
+    session = DBSession()
+    restaurantToDelete = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+      session.delete(restaurantToDelete)
+      flash('%s Successfully Deleted' % restaurantToDelete.name)
+      session.commit()
+      return redirect(url_for('showRestaurants', restaurant_id = restaurant_id))
+    else:
+      return render_template('deleteRestaurant.html',restaurant = restaurantToDelete)
 
+  finally:
+    if session:
+      session.close()
 
 #Show a restaurant menu
 @app.route('/restaurant/<int:restaurant_id>/')
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
+  try:
+    session = DBSession()
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
     return render_template('menu.html', items = items, restaurant = restaurant)
-     
+  finally:
+    if session:
+      session.close()
 
 
 #Create a new menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/new/',methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-  restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-  if request.method == 'POST':
-      newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form['price'], course = request.form['course'], restaurant_id = restaurant_id)
-      session.add(newItem)
-      session.commit()
-      flash('New Menu %s Item Successfully Created' % (newItem.name))
-      return redirect(url_for('showMenu', restaurant_id = restaurant_id))
-  else:
-      return render_template('newmenuitem.html', restaurant_id = restaurant_id)
+  try:
+    session = DBSession()
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form['price'], course = request.form['course'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        flash('New Menu %s Item Successfully Created' % (newItem.name))
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id = restaurant_id)
+  finally:
+    if session:
+      session.close()
 
 
 #Edit a menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit', methods=['GET','POST'])
 def editMenuItem(restaurant_id, menu_id):
-
+  try:
+    session = DBSession()
     editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
@@ -231,11 +318,16 @@ def editMenuItem(restaurant_id, menu_id):
         return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     else:
         return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = editedItem)
+  finally:
+    if session:
+      session.close()
 
 
 #Delete a menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods = ['GET','POST'])
 def deleteMenuItem(restaurant_id,menu_id):
+  try:
+    session = DBSession()
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     itemToDelete = session.query(MenuItem).filter_by(id = menu_id).one() 
     if request.method == 'POST':
@@ -245,6 +337,10 @@ def deleteMenuItem(restaurant_id,menu_id):
         return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     else:
         return render_template('deleteMenuItem.html', item = itemToDelete)
+
+  finally:
+    if session:
+      session.close()
 
 
 if __name__ == '__main__':
